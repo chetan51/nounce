a28a7bdb91d12fdde8be8741685b8eb222655708
Chetan Surpur <chetan51@gmail.com>
2011-02-12 20:23:06 -0800

    Created test application

---
+++ Test/TestApplication/NounceTestApplicationAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NounceTestApplicationAppDelegate.h
+//  NounceTestApplication
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NounceTestApplicationAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Test/TestApplication/NounceTestApplicationAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  NounceTestApplicationAppDelegate.m
+//  NounceTestApplication
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NounceTestApplicationAppDelegate.h"
+
+@implementation NounceTestApplicationAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
