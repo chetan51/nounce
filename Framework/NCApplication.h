51c0345f7ad408cc12357803f167f7af23ff0616
Chetan Surpur <chetan51@gmail.com>
2011-02-12 23:20:38 -0800

    Added NCApplication class to framework

---
+++ Framework/NCApplication.h
@@ -0,0 +1,18 @@
+//
+//  NCApplication.h
+//  Nounce
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NCApplication : NSObject {
+	NSNumber *ID;
+}
+
+@property (retain) NSNumber *ID;
+
+@end
---
+++ Framework/NCApplication.m
@@ -0,0 +1,16 @@
+//
+//  NCApplication.m
+//  Nounce
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NCApplication.h"
+
+
+@implementation NCApplication
+
+@synthesize ID;
+
+@end
