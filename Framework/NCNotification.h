9e12f58f1a023ea09f73233e04b5dcc0a627e91c
Chetan Surpur <chetan51@gmail.com>
2011-02-12 21:00:31 -0800

    Started NCNotification class for framework

---
+++ Framework/NCNotification.h
@@ -0,0 +1,16 @@
+//
+//  NCNotification.h
+//  Nounce
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NCNotification : NSObject {
+
+}
+
+@end
---
+++ Framework/NCNotification.m
@@ -0,0 +1,14 @@
+//
+//  NCNotification.m
+//  Nounce
+//
+//  Created by Chetan Surpur on 2/12/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NCNotification.h"
+
+
+@implementation NCNotification
+
+@end
