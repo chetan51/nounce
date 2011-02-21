12f1ee8c64340eb62c3d45a9b2c2daaac3b1868e
Chetan Surpur <chetan51@gmail.com>
2011-02-20 21:40:00 -0800

    Added NCNotificationCollectionViewItem prototype

---
+++ Application/NCNotificationCollectionViewItem.h
@@ -0,0 +1,17 @@
+//
+//  NCNotificationCollectionViewItem.h
+//  Nounce
+//
+//  Created by Chetan Surpur on 2/17/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NCNotificationCollectionViewItem : NSCollectionViewItem {
+}
+
+- (void) awakeFromNib;
+
+@end
