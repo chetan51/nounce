61d7537b068662c9d73ba655b9df6a235761f261
Chetan Surpur <chetan51@gmail.com>
2011-02-20 20:28:28 -0800

    Added more testing functionality

--- Test/TestApplication/NounceTestApplicationAppDelegate.m
+++ Test/TestApplication/NounceTestApplicationAppDelegate.m
@@ -17,6 +17,11 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	NCNotification *notification = [NCNotification notificationWithTitle:@"Test Notification" textContent:@"testing..."];
 	[NCNotificationManager notify:notification];
+	
+	NCNotification *notification2 = [NCNotification notificationWithTitle:@"Test Notification 2" textContent:@"testing again..."];
+	[[notification2 fromApp] setID:@"something"];
+	[[notification2 fromApp] setName:@"Another App"];
+	[NCNotificationManager notify:notification2];
 }
 
 @end
