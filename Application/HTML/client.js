3d897efa8d3ff04687af9fbb8a6b073c6c92728e
Chetan Surpur <chetan51@gmail.com>
2011-02-23 14:46:12 -0800

    Progress on WebView client

--- Application/HTML/client.js
+++ Application/HTML/client.js
@@ -4,11 +4,33 @@
 
 
 /*
+ * Global variables
+ */
+
+applicationViews = {};
+
+/*
+ * Accessors and setters of global variables
+ */
+
+function getApplicationView (ID) {
+	return applicationViews[ID];
+}
+
+function setApplicationView (applicationView) {
+	applicationViews[applicationView.ID] = applicationView;
+}
+
+/*
  * Initialization
  */
 
 $(document).ready(function() {
-	window.AppController.NPLog_("initialized client");
+	/* Testing
+	notify("0", "test notification", "testing ...", "TestApp", "Test Application");
+	notify("1", "test notification 2", "testing again ...", "TestApp", "Test Application");
+	notify("0", "test notification 3", "testing again 2 ...", "TestApp2", "Test Application 2");
+	*/
 });
 
 /*
@@ -17,5 +39,75 @@ $(document).ready(function() {
 
 function notify (notificationID, notificationTitle, notificationContent, fromAppID, fromAppName)
 {
-	window.AppController.NPLog_("notifying with title: " + notificationTitle);
+	var applicationView = getApplicationView(fromAppID);
+	var notificationView = new NotificationView(notificationID, fromAppID, notificationTitle, notificationContent);
+	
+	if (!applicationView) {
+		applicationView = new ApplicationView(fromAppID, fromAppName);
+		setApplicationView(applicationView);
+		applicationViews[fromAppID].setNotification(notificationView);
+	}
+
+	applicationView.updateDisplay();
+	notificationView.updateDisplay();
+}
+
+/*
+ * DOM accessors
+ */
+
+function getApplicationsDiv()
+{
+	return $("#applications");
+}
+
+/*
+ * View classes
+ */
+
+var ApplicationView = function (ID, name)
+{
+	var self = this;
+
+	self.ID            = ID;
+	self.name          = name;
+	self.notifications = {};
+	self.display       = null;
+	
+	self.setNotification = function (notification)
+	{
+		self.notifications[notification.ID] = notification;
+	}
+	
+	self.updateDisplay = function()
+	{
+		if (!self.display) {
+			self.display = $("#molds > .application").clone();
+			getApplicationsDiv().append(self.display);
+		}
+		
+		self.display.children(".name").html(self.name);
+	}
+}
+
+var NotificationView = function (ID, fromAppID, title, content)
+{
+	var self = this;
+
+	self.ID        = ID;
+	self.fromAppID = fromAppID;
+	self.title     = title;
+	self.content   = content;
+	self.display   = null;
+	
+	self.updateDisplay = function()
+	{
+		if (!self.display) {
+			self.display = $("#molds > .notification").clone();
+			getApplicationView(self.fromAppID).display.children(".notifications").append(self.display);
+		}
+
+		self.display.children(".title").html(self.title);
+		self.display.children(".content").html(self.content);
+	}
 }
