ba89daf1e59acbce16b54216c7c34bea4fc2fa45
Chetan Surpur <chetan51@gmail.com>
2011-02-28 00:40:58 -0800

    Linked Adium plugin against Nounce framework and added NCAISender class

---
+++ Plugins/NounceAdiumPlugin/NCAISender.h
@@ -0,0 +1,20 @@
+//
+//  NCAISender.h
+//  NounceAdiumPlugin
+//
+//  Created by Chetan Surpur on 2/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Nounce/NCNotification.h>
+
+
+@interface NCAISender : NSObject {
+	NSMutableArray *newMessages;
+	NCNotification *currentNotification;
+}
+
+@property (retain) NSMutableArray *newMessages;
+
+@end
---
+++ Plugins/NounceAdiumPlugin/NCAISender.m
@@ -0,0 +1,16 @@
+//
+//  NCAISender.m
+//  NounceAdiumPlugin
+//
+//  Created by Chetan Surpur on 2/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NCAISender.h"
+
+
+@implementation NCAISender
+
+@synthesize newMessages;
+
+@end
